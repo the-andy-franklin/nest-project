572eb14192140ba26f411f6a9a7f544375fe23cb
Andy <the.andy.franklin@gmail.com>
2023-06-25 17:09:24 -0700

    pizza has pizza

--- src/pizza/pizza.controller.ts
+++ src/pizza/pizza.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import type { Response } from 'express';
 
 @Controller('pizza')
 export class PizzaController {
   @Get()
-  getPizza(@Res() res: Response) {
-    return res.json({ message: 'Hello Pizza!' });
+  @Render('pizza')
+  getPizza() {
+    return { message: 'Hello Pizza!' };
   }
 }
